7508323b7f64096a117b6c4c66c0bbf2a146d8a8
dinhvanit <nguyendinhvanefto@gmail.com>
2024-10-09 10:00:34 +0700

    test

---
+++ commandlineVesion/src/Library_Manager/Library.java
@@ -0,0 +1,11 @@
+package Library_Manager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Document> documents = new ArrayList<>();
+    private List<User> users = new ArrayList<>();
+
+    public void updateDocument(S)
+}
